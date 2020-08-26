678f7b2e4a9a4ab1e3718093a478fca83b07a07f
Nelson Segarra <junicus@outlook.com>
2020-08-26 12:58:53 -0400

    start of a design system

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-essentials",
+    "@storybook/addon-knobs"
+  ]
+}
